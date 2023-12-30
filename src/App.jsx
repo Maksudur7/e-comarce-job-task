d2e268ee0ee61eeac77ddb15f3cbb7966af70ce9
Maksudr <maksudurrahamanmishu7@gmail.com>
2023-12-30 19:03:46 +0600

    create a vite project and fetch dummyjson products

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './NavBar/Home paige/Home'
+import Navbar from './NavBar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
